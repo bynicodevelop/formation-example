72d822ed3c86f65baecf2f3163338ad55ba1bc0a
nico-develop <contact@nico-develop.fr>
2021-01-08 13:51:51 +0100

    Updated index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          Welcome NuxtJS & Firebase
+          List des utilisateur de la formation : NuxtJS & Firebase
         </v-card-title>
         <v-card-text>
           <ul>
